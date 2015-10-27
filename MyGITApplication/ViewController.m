72757a5a8e93f6b0325d3649eef7a7f384b9946c
Sanjeev_Bharti <sanjeev_bharti@choicehotels.com>
2015-10-27 16:47:17 -0700

    Code cleaning
    Code has cleaned

--- MyGITApplication/ViewController.m
+++ MyGITApplication/ViewController.m
@@ -17,12 +17,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    NSLog(@"This is test message");
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
 
 @end
